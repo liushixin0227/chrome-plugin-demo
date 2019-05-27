b7dc908851aee3217ae850c767fa519e29eba1bd
shxliu <522273565@qq.com>
2019-05-27 20:18:05 +0800

    feat(content-script.js):增加功能 添加jquery库 新增非法标题提醒功能 新增MO等级判定

--- simple-chrome-plugin-demo/content-script.js
+++ simple-chrome-plugin-demo/content-script.js
@@ -1,3 +1,55 @@
-﻿(function() {
-	console.log('这是 simple-chrome-plugin-demo 的content-script！');
+﻿(function () {
+    'use strict';
+    const AssistAudit = {
+        TitleProcess: function () {
+            try {
+                const TitleElement = document.getElementsByClassName("ant-input")[0];
+                const TitleStr = TitleElement.getAttribute("value");
+
+                //判读标题是否合法,如果标题违法,变成红色
+                if (TitleStr) {
+                    const StyleStr = TitleElement.getAttribute("style") + "color: red;";
+                    TitleElement.setAttribute("style", StyleStr);
+                    console.log("修改状态成功!");
+                }
+            } catch (error) {
+                console.log("出错了");
+            }
+        },
+        OMLevelProcess: function () {
+            try {
+                const liElements = document.getElementsByClassName("ant-card _3DPw4 ant-card-bordered")[0]
+                    .getElementsByTagName("li");
+                //判读标题是否合法,如果标题违法,变成红色
+                for (let notDel_counter = 0; notDel_counter < liElements.length; notDel_counter++) {
+                    let Tag = liElements[notDel_counter].outerText.split(':')[0];
+                    if (Tag === "OM 等级") {
+                        if (int(liElements[notDel_counter].getElementsByTagName("label")) > 2) {
+                            liElements[notDel_counter].getElementsByTagName("span")[0].setAttribute("style", "color: yellow");
+                            console.log("修改状态成功!");
+                            break;
+                        }
+                    }
+                }
+            } catch (error) {
+                console.log('出错了');
+            }
+        },
+        Process: function () {
+            this.TitleProcess();
+            this.OMLevelProcess();
+        }
+    };
+
+    $(document).ready(function () {
+        AssistAudit.Process();
+
+        //为防止ajax异步延时加载的广告隔2s再清除一次
+        setTimeout(function () {
+            AssistAudit.Process();
+        }, 2000)
+    });
 })();
+
+
+
